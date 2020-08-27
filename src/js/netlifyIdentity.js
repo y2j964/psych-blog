77b940aaa0919fca67b3dc2f77339199d7d12fd9
Justin Mooney <y2j964@aol.com>
2020-08-27 02:37:50 -0400

    Introduce netifly support

---
+++ src/js/netlifyIdentity.js
@@ -0,0 +1,13 @@
+const initNetlifyIdentity = () => {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", (user) => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+};
+
+export default initNetlifyIdentity;
