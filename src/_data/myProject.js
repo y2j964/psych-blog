cad7787ac112c47f17a04a489bd2400d8833dbee
Justin Mooney <y2j964@aol.com>
2020-08-27 21:23:33 -0400

    Remove gh-pages deploy
    Deployment will be handled be Netlify.

--- src/_data/myProject.js
+++ src/_data/myProject.js
@@ -3,6 +3,5 @@ module.exports = {
   rootPath:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://y2j964.github.io/psych-blog",
-  // ending prefix should match prefix added in package.json build script
+      : "https://stoic-joliot-29217f.netlify.app/",
 };
