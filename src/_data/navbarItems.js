58c1845a0968c081b3a14b0fc3a6a041edf6e7e6
Justin Mooney <y2j964@aol.com>
2020-09-29 16:03:36 -0400

    Make navbar implementation more DRY
    Having a loop here reduces the amount of repetition we have in our code.
    This will particularly be useful when we introduce active link logic.

---
+++ src/_data/navbarItems.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    text: "Psychic Voyages",
+    slug: "/psychic-voyages/",
+    // isActive: (page) => page.url === "/psychic-voyages/",
+  },
+  {
+    text: "Media",
+    slug: "/media/",
+    // isActive: (page) => page.url === "/media/",
+  },
+  {
+    text: "Support",
+    slug: "/support/",
+    // isActive: (page) => page.url === "/contact/",
+  },
+  {
+    text: "About Me",
+    slug: "/about-me/",
+    // isActive: (page) => page.url === "/about-me/",
+  },
+  {
+    text: "Contact",
+    slug: "/contact/",
+    // isActive: (page) => page.url === "/contact/",
+  },
+];
